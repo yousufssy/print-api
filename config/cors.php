127938d179bc644ea82e2d69c69c553685d1e61e
yousufssy <yousufsaty@gmail.com>
2026-04-09 04:22:48 +0300

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths'                    => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods'          => ['*'],
-    'allowed_origins'          => [env('FRONTEND_URL', 'http://localhost:5173')],
+    'allowed_origins'          => [env('FRONTEND_URL', 'https://print-app-production.up.railway.app')],
     'allowed_origins_patterns' => [],
     'allowed_headers'          => ['*'],
     'exposed_headers'          => [],
